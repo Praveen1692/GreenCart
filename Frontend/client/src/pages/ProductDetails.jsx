d1166910c02fa01c7a631d4095b8a8a1862af391
Praveen <praying.sharma@gmail.com>
2025-05-07 12:21:58 +0530

    Add

--- Frontend/client/src/pages/ProductDetails.jsx
+++ Frontend/client/src/pages/ProductDetails.jsx
@@ -111,9 +111,10 @@ function ProductDetails() {
 
         {/*related Products */}
 
-        <div>
-          <div>
+        <div className="flex flex-col items-center mt-20">
+          <div className="flex flex-col items-center w-max">
             <p>Related Products</p>
+            <div className="w-20 h-0.5 bg-primary rounded-full mt-2"></div>
           </div>
 
         </div>
