96eda3b238eaa09be1c80055fe39942dbaf0a7c2
Praveen <praying.sharma@gmail.com>
2025-05-08 21:32:25 +0530

    Orders

--- Frontend/client/src/pages/MyOrders.jsx
+++ Frontend/client/src/pages/MyOrders.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { dummyOrders } from "../assets/assets";
 
 const MyOrders = () => {
+  const [myOrders, setMyOrders] = useState([]);
+
+  const fetchMyOrders = async () => {
+    setMyOrders(dummyOrders);
+  };
+  useEffect(() => {
+    fetchMyOrders();
+  }, [myOrders]);
   return (
     <div className="mt-16 pb-16">
       <div className="flex flex-col items-end w-max mb-8">
         <p className="text-2xl font-medium uppercase">My orders</p>
         <div className="w-16 h-0.5 bg-primary rounded-full"></div>
       </div>
+      {myOrders.map((order, index) => (
+        <div>
+          <p>
+            <span></span>
+          </p>
+        </div>
+      ))}
     </div>
   );
 };
