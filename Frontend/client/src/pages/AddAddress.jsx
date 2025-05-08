d2a0b0b326ecde638b85708d25f7798c0ccc9afb
Praveen <praying.sharma@gmail.com>
2025-05-08 15:18:37 +0530

    Address

--- Frontend/client/src/pages/AddAddress.jsx
+++ Frontend/client/src/pages/AddAddress.jsx
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/assets";
 
 // Input Field Component;
-const inputFiled = () => <input type="text" />;
+const InputFiled = ({ type, placeholder, name, handleChange, address }) => (
+  <input
+    className="w-full px-2 py-2.5 border border-gray-500/30 rounded outline-none text-gray-500 focus:border-primary transition"
+    type={type}
+    placeholder={placeholder}
+    name={name}
+    onChange={handleChange}
+    value={address[name]}
+    required
+  />
+);
 
 const AddAddress = () => {
-    const onSubmitHandler=()=>{
-        
-    }
+  const [address, setAddress] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    country: "",
+    phone: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setAddress((prevAddress) => ({
+      ...prevAddress,
+      [name]: value,
+    }));
+  };
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="mt-16 pb-16">
       <p className="text-2xl md:text-3xl text-gray-300">
@@ -15,11 +46,75 @@ const AddAddress = () => {
       </p>
       <div className="flex flex-col-reverse md:flex-row justify-between mt-10">
         <div className="flex-1 max-w-md">
-            <form onSubmit={onSubmitHandler}>
+          <form onSubmit={onSubmitHandler} className="space-y-3 mt-6 text-sm">
+            <div className="grid grid-cols-2 gap-4">
+              <InputFiled
+                handleChange={handleChange}
+                address={address}
+                name="first name"
+                type="text"
+                placeholder="First name"
+              />
+              <InputFiled
+                handleChange={handleChange}
+                address={address}
+                name="last name"
+                type="text"
+                placeholder="Last name"
+              />
+            </div>
+            <InputFiled
+              handleChange={handleChange}
+              address={address}
+              name="email"
+              type="email"
+              placeholder="Email address"
+            />
 
-            </form>
+            <InputFiled
+              handleChange={handleChange}
+              address={address}
+              name="street"
+              type="text"
+              placeholder="street"
+            />
 
+            <div className="grid grid-cols-2 gap-4">
+              <InputFiled
+                handleChange={handleChange}
+                address={address}
+                name="state"
+                type="text"
+                placeholder="State"
+              />
 
+              <InputFiled
+                handleChange={handleChange}
+                address={address}
+                name="city"
+                type="text"
+                placeholder="City"
+              />
+            </div>
+
+            <div className="grid grid-cols-2 gap-4">
+              <InputFiled
+                handleChange={handleChange}
+                address={address}
+                name="zipcode"
+                type="number"
+                placeholder="Zip Code"
+              />
+
+              <InputFiled
+                handleChange={handleChange}
+                address={address}
+                name="country"
+                type="text"
+                placeholder="Country"
+              />
+            </div>
+          </form>
         </div>
         <img
           className="md:mr-16 mb-16 md:mt-0"
