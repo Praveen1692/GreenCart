d83695233b6bc77b4bf3475678118b1c01f51506
Praveen <praying.sharma@gmail.com>
2025-05-08 21:51:44 +0530

    Orders

--- Frontend/client/src/pages/MyOrders.jsx
+++ Frontend/client/src/pages/MyOrders.jsx
@@ -17,10 +17,29 @@ const MyOrders = () => {
         <div className="w-16 h-0.5 bg-primary rounded-full"></div>
       </div>
       {myOrders.map((order, index) => (
-        <div>
-          <p>
-            <span></span>
+        <div
+          key={index}
+          className="border border-gray-300 rounded-lg mb-10 p-4 py-5 max-w-4xl"
+        >
+          <p className="flex justify-between md:items-center text-gray-400 md:font-medium max-md:flex-col">
+            <span>OrderId:{order._id}</span>
+            <span>Payment:{order.paymentType}</span>
+            <span>Total Amount:${order.amount}</span>
           </p>
+          {order.items.map((item,index)=>(
+            <div>
+                <div className="flex items-center mb-4 md:mb-0">
+                    <div className="bg-primary/10 p-4 rounded-lg">
+                        <img src={item.product.image[0]} alt="Product Image" className="w-16 h-16" />
+                    </div>
+                    <div className="ml-4">
+                        <h2 className="text-xl font-medium text-gray-800">{item.product.name}</h2>
+                        <p>Category:{item.product.category}</p>
+                    </div>
+                </div>
+            </div>
+
+          ))}
         </div>
       ))}
     </div>
