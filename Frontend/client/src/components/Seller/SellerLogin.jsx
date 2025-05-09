71f595d3766547f787a20bd951026c145ab6d730
Praveen <praying.sharma@gmail.com>
2025-05-09 15:57:29 +0530

    Seller

--- Frontend/client/src/components/Seller/SellerLogin.jsx
+++ Frontend/client/src/components/Seller/SellerLogin.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useAppContext } from "../../context/AppContext";
 
 function SellerLogin() {
+  const { isSeller, setIsSeller, navigate } = useAppContext();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    if (isSeller) {
+      navigate("/seller");
+    }
+  }, [isSeller]);
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    setIsSeller(true);
+  };
+
   return (
-    <div>SellerLogin</div>
-  )
+    !isSeller && (
+      <form
+        onSubmit={onSubmitHandler}
+        className="min-h-screen flex items-center text-sm text-gray-600"
+      >
+        <div>
+          <p className="text-2xl font-medium m-auto">
+            <span className="text-primary">Seller</span>Login
+          </p>
+        </div>
+      </form>
+    )
+  );
 }
 
-export default SellerLogin
+export default SellerLogin;
