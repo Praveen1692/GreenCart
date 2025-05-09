903ed74b42e0758cecbd56a8906be2ede5cd6ce9
Praveen <praying.sharma@gmail.com>
2025-05-09 21:22:27 +0530

    dd

--- Frontend/client/src/components/Seller/SellerLogin.jsx
+++ Frontend/client/src/components/Seller/SellerLogin.jsx
@@ -23,10 +23,18 @@ function SellerLogin() {
         onSubmit={onSubmitHandler}
         className="min-h-screen flex items-center text-sm text-gray-600"
       >
-        <div>
+        <div className="flex flex-col gap-5 m-auto items-start p-8 py-12 min-w-80 sm:min-w-80 rounded-lg shadow-xl border border-gray-200">
           <p className="text-2xl font-medium m-auto">
             <span className="text-primary">Seller</span>Login
           </p>
+          <div className="w-full">
+            <p>Email</p>
+            <input type="email" name="email" placeholder="Enter Your Email..." />
+          </div>
+          <div className="w-full">
+            <p>Password</p>
+            <input type="password" name="password" placeholder="Enter Your Password.." />
+          </div>
         </div>
       </form>
     )
