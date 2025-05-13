9a506d0001de4830f0a97b56096e094fe466d13c
Praveen <praying.sharma@gmail.com>
2025-05-13 11:35:24 +0530

    add

--- Server/Server.js
+++ Server/Server.js
@@ -1,12 +1,20 @@
+import cookieParser from "cookie-parser";
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
-
-
+const allowedOrigins = ["http://localhost:5173"];
 
 // middleware configuration;
-
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
 let PORT = process.env.PORT || 4000;
 
@@ -14,7 +22,6 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello Server  API</h1>");
 });
 
-app.listen(PORT,()=>{
-    console.log(`server running at PORT: ${PORT}`);
-    
-})
+app.listen(PORT, () => {
+  console.log(`server running at PORT: ${PORT}`);
+});
