b030efa5e3642b936a3df21eb8fbdb1937706129
Praveen <praying.sharma@gmail.com>
2025-05-21 16:42:57 +0530

    Product Schema

---
+++ Server/controllers/productController.js
@@ -0,0 +1,11 @@
+//Add->: /api/product/add
+export const addProduct = async (req, res) => {};
+
+//Product List->: /api/product/list
+export const productList = async (req, res) => {};
+
+//Product By Id->: /api/product/:id;
+export const productById = async (req, res) => {};
+
+// Change Stock->: /api/product/stock
+export const changeStock = async (req, res) => {};
---
+++ Server/models/Product.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    
+  },
+  {
+    timestamps: true,
+  }
+);
