1bbc37f0ba28e12a22bedd7ab7f740831708b199
Praveen <praying.sharma@gmail.com>
2025-05-21 08:23:17 +0530

    seller auth

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { isAuth, login, register } from "../controllers/userControllers.js";
+import {
+  isAuth,
+  login,
+  logout,
+  register,
+} from "../controllers/userControllers.js";
 import authUser from "../middleware/authUser.js";
 const userRouter = express.Router();
 
 userRouter.post("/register", register);
 userRouter.post("/login", login);
-userRouter.get("/is-auth",authUser ,isAuth);
+userRouter.get("/is-auth", authUser, isAuth);
+userRouter.get("/logout", authUser, logout);
 
 export default userRouter;
